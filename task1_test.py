84ecbe9afd47a5612c1e4bd6b1e28bba6e2383b7
Lj <55156461+Usercodehappen@users.noreply.github.com>
2021-03-01 09:09:55 -0800

    update

--- task1_test.py
+++ task1_test.py
@@ -6,4 +6,10 @@ def test1():
   assert task1.sum(11,2.5) == 13.5
 
 def test2():
-  assert task1.sum(8,-2) == 6
+  assert task1.sum(8,-2) == 
+#this should return a value of 7  
+x = sum(3,4)
+print(x)
+#this should return a value of 12.5
+y = sum(11,1.5)
+print(y)
